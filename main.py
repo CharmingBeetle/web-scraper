713d1e43090dc76d077201021269101c22322c0b
CharmingBeetle <nina.e.thomas@gmail.com>
2025-08-28 21:30:13 +0100

    reformat + add main func

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ import httpx
 from selectolax.parser import HTMLParser
 
 # web scraper tutorial
+def get_html(base_url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36"
+        }
 
-url = "https://books.toscrape.com/"
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36"}
-
-response = httpx.get(url, headers=headers) 
-# print(response.text) #text of url request
-
-html = HTMLParser(response.text) # query this to find data you want
-
-# print(html.css_first("title").text()) #finds first css selector matching title
+    response = httpx.get(base_url, headers=headers) 
+    # print(response.text) #text of url request
+    html = HTMLParser(response.text) # query this to find data you want
+    return html
 
 # function to extract text, if no text, return none
 def extract_text(html, selector, attribute=None):
@@ -37,18 +36,27 @@ def extract_rating(html, selector):
     except AttributeError:
         return None
 
-books = html.css("article.product_pod") #target all books
-# print(books)
+def parse_page(html):
+    books = html.css("article.product_pod") #target all books
+    print(books)
+    
+    # loop through books and print title
+    for book in books: 
+        # print(book.css_first("h3 > a").text())
 
-# loop through books and print title
-for book in books: 
-    # print(book.css_first("h3 > a").text())
+        #create item dictionary
+        item = {
+            "name": extract_text(book, "h3 > a"),
+            "price": extract_text(book, "p.price_color"),
+            "rating": extract_rating(book, "p.star-rating"),
+            "url": extract_text(book, "h3 > a", "href")
+        }
+        print(item)
 
-  #create item dictionary
-    item = {
-        "name": extract_text(book, "h3 > a"),
-        "price": extract_text(book, "p.price_color"),
-        "rating": extract_rating(book, "p.star-rating"),
-        "url": extract_text(book, "h3 > a", "href")
-    }
-    print(item)
+def main():
+    base_url = "https://books.toscrape.com/"
+    html = get_html(base_url)
+    parse_page(html)
+
+if __name__ == "__main__":
+    main()
