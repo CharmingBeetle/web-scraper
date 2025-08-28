16fee7892cb643a3c08387432e119389155f3170
CharmingBeetle <nina.e.thomas@gmail.com>
2025-08-28 19:47:18 +0100

    set up venv + confirm response

---
+++ main.py
@@ -0,0 +1,12 @@
+import httpx 
+from selectolax.parser import HTMLParser
+
+# web scraper tutorial
+
+url = "https://books.toscrape.com/"
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36"}
+
+response = httpx.get(url, headers=headers) 
+print(response) # confirm response
+
+
